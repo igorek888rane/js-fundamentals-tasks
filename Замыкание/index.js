8914dcf6a1303a5366cd9fa1d56a94b0d7968943
Igor Tsuker <igorek888rane@gmail.com>
2023-01-08 16:50:51 +0300

    Замыкание

---
+++ Замыкание/index.js
@@ -0,0 +1,11 @@
+const addByX = numOne => (numTwo) => numOne + numTwo
+
+
+const addByTwo = addByX(2)
+console.log(addByTwo(3));
+console.log(addByTwo(5));
+
+const addByFour = addByX(4)
+console.log(addByFour(3));
+console.log(addByFour(5));
+
