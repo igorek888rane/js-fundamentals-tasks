24d4555fcd021a142302d2dbca7dcd03a55f7a81
Igor Tsuker <igorek888rane@gmail.com>
2023-01-09 18:47:25 +0300

    Цензор

--- Цензор/index.js
+++ Цензор/index.js
@@ -1,23 +1,40 @@
-const censor = () => {
+
+//С рекурсией
+const censorWith = () => {
     const censorParams = []
-    return function censorName(oneParam, secondParam = 0) {
-        if (secondParam !== 0) {
+    return function censorPhrase(oneParam, secondParam = '') {
+        if (secondParam) {
             censorParams.push([oneParam, secondParam])
         } else {
             let censorEl = censorParams.pop()
             if (censorEl === undefined) {
                 return oneParam
             }
-            return censorName( oneParam.replaceAll(censorEl[0], censorEl[1]))
+            return censorPhrase( oneParam.replaceAll(censorEl[0], censorEl[1]))
 
         }
     }
 }
-const changeScene = censor();
+//Без рекурсии
+const censor = () => {
+    const censorParams = []
+    return (oneParam, secondParam = '') => {
+        if (secondParam) {
+            censorParams.push([oneParam, secondParam])
+        } else {
+            censorParams.forEach(el => {
+                oneParam = oneParam.replaceAll(el[0], el[1])
+            })
+            return oneParam
+        }
+    }
+}
+const changeScene = censorWith();
 changeScene('PHP', 'JS');
 
 changeScene('backend', 'frontend');
 
 
 console.log(changeScene('PHP  is the most popular programming language for backend web-development'))
+
 
